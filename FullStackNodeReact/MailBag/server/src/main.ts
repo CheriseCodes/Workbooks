af749329dc9f813f6683fba2ee0706aeaa35f96e
CheriseCodes <cherise.bry@gmail.com>
2022-03-08 19:00:21 -0500

    upload node/react files

---
+++ FullStackNodeReact/MailBag/server/src/main.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import express,
+    { Express, NextFunction, Request, Response } from 'express';
+import { serverInfo } from './ServerInfo';
+import * as IMAP from './IMAP';
+import * as SMTP from './SMTP';
+import * as Contacts from './Contacts';
+import { IContCT } from './Contacts';
+
+const app: Express = express();
+
+app.use(express.json());
+
+/*
+express.static is used to serve static resources. Just need to tell express
+where those resources are.
+*/
+app.use("/",
+    express.static(path.join(__dirname, "../../client/dist"))
+);
+
+/*
+*/
+app.use(function(inRequest: Request, inResponse: Response, inNext: NextFunction){
+    inResponse.header("Access-Control-Allow-Origin", "*");
+    inResponse.header("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
+    inResponse.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+    inNext();
+});
+
+
