93acb13226455d5064cf3faf8590074ec80fe70e
CheriseCodes <cherise.bry@gmail.com>
2022-03-09 12:28:59 -0500

    completed ServerInfo.ts for MailBag

---
+++ FullStackNodeReact/MailBag/server/src/ServerInfo.ts
@@ -0,0 +1,19 @@
+const path = require("path");
+const fs = require("fs");
+
+export interface IServerInfo {
+    smtp: {
+        host: string, port: number,
+        auth: {user: string, pass:string}
+    },
+    imap: {
+        host: string, port: number,
+        auth: {user: string, pass: string}
+    }
+}
+export let serverInfo: IServerInfo;
+
+// load data from serverInfo.json into the serverInfo variable
+const rawInfo: string = fs.readFileSync(path.join(__dirname, "../serverInfo.json"));
+serverInfo = JSON.parse(rawInfo);
+
