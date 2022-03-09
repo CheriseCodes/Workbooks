53531c64478dfb8c672c1fc3e5603a6e5a9f5e07
CheriseCodes <cherise.bry@gmail.com>
2022-03-09 12:00:51 -0500

    complete rest endpoints for MailBag

--- FullStackNodeReact/MailBag/server/src/main.ts
+++ FullStackNodeReact/MailBag/server/src/main.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import express,
-    { Express, NextFunction, Request, Response } from 'express';
+    { Express, NextFunction, request, Request, Response } from 'express';
 import { serverInfo } from './ServerInfo';
 import * as IMAP from './IMAP';
 import * as SMTP from './SMTP';
@@ -20,6 +20,11 @@ app.use("/",
 );
 
 /*
+Enables API endpoints to be accessed from any domain
+Middlewares
+- is defined by supplying a function to app.use()
+- function must take in incoming request, generated response, a reference to the next function
+- form a chain when Express handles a request
 */
 app.use(function(inRequest: Request, inResponse: Response, inNext: NextFunction){
     inResponse.header("Access-Control-Allow-Origin", "*");
@@ -28,4 +33,115 @@ app.use(function(inRequest: Request, inResponse: Response, inNext: NextFunction)
     inNext();
 });
 
+/*
+- Retrieving a resource should use the GET method
+- callback will be executed when the request is received
+*/
+app.get("/mailboxes",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const mailboxes: IMAP.IMailbox[] = await imapWorker.listMailboxes();
+            inResponse.json(mailboxes);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
 
+app.get("/mailboxes/:mailbox",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messages: IMAP.IMessage[] = await imapWorker.listMessages({ mailbox: inRequest.params.mailbox});
+            inResponse.json(messages);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+/*
+- messageBody is sent as plain text with Response.send method
+- client is responsible for HTML message bodies
+*/
+app.get("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            const messageBody: string = await imapWorker.getMessageBody({
+                mailbox: inRequest.params.mailbox,
+                id: parseInt(inRequest.params.id, 10)
+            });
+            inResponse.send(messageBody);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/messages/:mailbox/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+            imapWorker.deleteMessage({
+                mailbox: inRequest.params.mailbox,
+                id: parseInt(inRequest.params.id, 10)
+            });
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/message",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
+            // inRequest.body includes dest. address, subject, message body
+            await smtpWorker.sendMessage(inRequest.body);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.get("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contacts: IContact[] = await contactsWorker.listContacts();
+            inResponse.json(contacts);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.post("/contacts",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            const contact: IContact = await contactsWorker.addContact(inRequest.body);
+            // Resturning contact to allow contact to save generated ID. This ID can be used later
+            // for deleting or updating the contact. This is a common REST API pattern
+            inResponse.json(contact);
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
+
+app.delete("/contacts/:id",
+    async (inRequest: Request, inResponse: Response) => {
+        try {
+            const contactsWorker: Contacts.Worker = new Contacts.Worker();
+            await contactsWorker.deleteContact(inRequest.params.id);
+            inResponse.send("ok");
+        } catch (inError) {
+            inResponse.send("error");
+        }
+    }
+);
